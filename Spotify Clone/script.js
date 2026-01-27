09821e0cb033da78539ec4f532b524429d2b17ea
Rohan Mendon <rohanmendon26@gmail.com>
2026-01-27 21:16:36 +0530

    Clone in progress

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -1,1 +1,43 @@
 console.log("let's start javascript")
+
+async function getsongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mpeg")) {
+            songs.push(element.href)
+        }
+
+    }
+    return songs
+
+}
+
+async function main() {
+    //get the list of all songs
+    let songs= await getsongs()
+    console.log(songs)
+
+    let songUL=document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML=songUL.innerHTML + `<li>${song}</li>`
+        
+    }
+    
+    //play the first song
+    var audio=new Audio(songs[0]);
+    // audio.play();
+
+    audio.addEventListener("loadeddata",()=>{
+             let duration=audio.duration;
+             console.log(duration);
+             
+    })
+}
+ 
+main();
