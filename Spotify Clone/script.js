57f114c62a8a3742cc9fe243223e4b7666a7dda7
Rohan Mendon <rohanmendon26@gmail.com>
2026-02-12 21:41:50 +0530

    Clone in progress

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -117,7 +117,15 @@ document.querySelector(".seekbar").addEventListener("click", e => { let percent 
 });
 
 
+//Add an event listener for hamburger
+document.querySelector(".hamburger").addEventListener("click",()=>{
+    document.querySelector(".left").style.left="0"
+})
 
+//Add an event listener for close button
+document.querySelector(".close").addEventListener("click",()=>{
+    document.querySelector(".left").style.left="-110%"
+})
 
 
 }
