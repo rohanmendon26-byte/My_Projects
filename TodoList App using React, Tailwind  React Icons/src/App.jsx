e3c6d5722c46d5de6bcdf3af8c4632bbfb03acb7
Rohan Mendon <rohanmendon26@gmail.com>
2026-05-31 21:54:31 +0530

    completed

--- TodoList App using React, Tailwind  React Icons/src/App.jsx
+++ TodoList App using React, Tailwind  React Icons/src/App.jsx
@@ -1,4 +1,3 @@
-//Main file
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -12,10 +11,22 @@ function App() {
   return (
     <>
     <Navbar/>
-      <div className="container">
-        <div className="bg-red-600">
-          <h1>Your Todos</h1>
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[70vh]">
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input className='bg-white' type="text"/>
+          <button className='bg-violet-800 hover:bg-violet-950 p-3 py-1'>Add</button>
         </div>
+          <h2  className="text-2xl font-bold">Your Todos</h2>
+          <div className="todos">
+            <div className="todo flex">
+               <div className="text">Lorem, ipsum dolor sit amet consectetur adipisicing.</div>
+               <div className="buttons">
+                  <button>Edit</button>
+                  <button>Delete</button>
+               </div>
+            </div>
+          </div>
       </div>
     </>
   )
--- TodoList App using React, Tailwind  React Icons/src/Components/Navbar.jsx
+++ TodoList App using React, Tailwind  React Icons/src/Components/Navbar.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-slate-700 text-white">
-        <ul className='flex'>
-            <li>Home</li>
-            <li>Your Tasks</li>
+    <nav className="flex justify-between bg-blue-900 text-white py-2">
+      <div className="logo">
+        <span className='font-bold text-xl mx-8 my-2'>iTask</span>
+      </div>
+        <ul className='flex gap-8 mx-9'>
+            <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+            <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
         </ul>
     </nav>
   )
