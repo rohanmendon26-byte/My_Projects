9bd6da544ce0265e3777e1df31859fa97cb12f0e
Rohan Mendon <rohanmendon26@gmail.com>
2026-06-14 21:16:44 +0530

    completed

--- TodoList App using React, Tailwind  React Icons/src/App.jsx
+++ TodoList App using React, Tailwind  React Icons/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Navbar from './Components/Navbar'
 import './App.css'
+import { v4 as uuidv4 } from "uuid";
+
 
 function App() {
 
@@ -22,8 +24,23 @@ function App() {
      setTodo(e.target.value)
   }
 
+  const handleCheckbox = (e) => {
+    console.log(e,e.target)
+  let id = e.target.name;
+  console.log(`The id is ${id}`)
+  let index = todos.findIndex(item => {
+    return item.id === id
+  });
+  console.log(index)
+  let newTodos =todos;
+  newTodos[index].isCompleted =!newTodos[index].isCompleted;
+  setTodos(newTodos);
+  console.log(newTodos)
+};
+  
+
   const handleAdd=()=>{
-    setTodos([...todos,{todo,isCompleted:false}])
+    setTodos([...todos,{id:uuidv4(),todo,isCompleted:false}])
     setTodo("")
     console.log(todos)
     }
@@ -40,11 +57,12 @@ function App() {
         </div>
           <h2  className="text-2xl font-bold">Your Todos</h2>
           <div className="todos">
-            {todos.map(item=>{
+            {todos.map(item=>{    
 
-           
-            return <div key={todo} className="todo flex">
-               <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
+            return <div key={item.id} className="todo flex w-1/4 my-3 justify-between">
+              <input name={item.id} onChange={handleCheckbox}  type="checkbox" checked={item.isCompleted}  id="" />
+               <div className={item.isCompleted?"":"line-through"}>{item.todo}
+                </div>
                <div className="buttons">
                   <button onClick={handleEdit}className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-xl mx-1'>Edit</button>
                   <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-xl mx-1'>Delete</button>
